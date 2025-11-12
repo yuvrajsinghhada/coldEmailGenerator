2a8c909e35dd718c7bf5db723b21a0b8c87ed0af
YUVRAJ SINGH HADA <yuvrajsinghhada08@gmail.com>
2025-11-12 16:54:30 +0530

    Add files via upload

---
+++ chains.py
@@ -0,0 +1,59 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Chain:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.3-70b-versatile")
+
+    def extract_jobs(self, cleaned_text):
+        prompt_extract = PromptTemplate.from_template(
+            """
+            ### SCRAPED TEXT FROM WEBSITE:
+            {page_data}
+            ### INSTRUCTION:
+            The scraped text is from the career's page of a website.
+            Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills` and `description`.
+            Only return the valid JSON.
+            ### VALID JSON (NO PREAMBLE):
+            """
+        )
+        chain_extract = prompt_extract | self.llm
+        res = chain_extract.invoke(input={"page_data": cleaned_text})
+        try:
+            json_parser = JsonOutputParser()
+            res = json_parser.parse(res.content)
+        except OutputParserException:
+            raise OutputParserException("Context too big. Unable to parse jobs.")
+        return res if isinstance(res, list) else [res]
+
+    def write_mail(self, job, links):
+        prompt_email = PromptTemplate.from_template(
+            """
+            ### JOB DESCRIPTION:
+            {job_description}
+
+            ### INSTRUCTION:
+            You are Yuvraj, a final-year Computer Science and Engineering student at NIT Jalandhar, 
+            skilled in software development and AI-driven problem-solving. 
+            Write a professional email to the client or recruiter regarding the job mentioned above. 
+            Highlight your technical strengths, academic background, and enthusiasm for the opportunity. 
+            Use the provided portfolio links ({link_list}) to showcase relevant work or projects that demonstrate your capabilities. 
+            Maintain a professional and engaging tone. 
+            Remember you are Yuvraj, . final year Computer Science and Engineering student at NIT Jalandhar.
+            Do not provide a preamble.
+            ### EMAIL (NO PREAMBLE):
+
+            """
+        )
+        chain_email = prompt_email | self.llm
+        res = chain_email.invoke({"job_description": str(job), "link_list": links})
+        return res.content
+
+if __name__ == "__main__":
+    print(os.getenv("GROQ_API_KEY"))
